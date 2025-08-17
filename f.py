f5ab7e2e0d1914ad2ce5fabc9d66b7f1d36790c4
Sergey Mishin <soje-poje@hotmail.com>
2025-08-17 17:02:45 +0300

    два задания по  f-строкам и конкатенации

---
+++ f.py
@@ -0,0 +1,9 @@
+name = "Sergey"
+age = 33
+
+# print(f"Меня зовут, {name} и мне {age} лет")
+
+num_1 = 10
+num_2 = 20
+
+print(f"Сумма чисел равна {num_1 + num_2}")
---
+++ string_2.py
@@ -0,0 +1,7 @@
+str_1 = "hello"
+
+
+print(str_1)
+print(dir(str_1))
+
+print(str_1.upper())
